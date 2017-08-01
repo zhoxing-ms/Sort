ca84fd703bc92dbd4f692317662d0d16401cfaef
zhouxing <244266602@qq.com>
2017-08-01 14:25:26 +0800

    Update InsertSort.java

--- InsertSort.java
+++ InsertSort.java
@@ -1,24 +1,30 @@
 package Sort;
 
+/**
+ 插入排序
+ 时间复杂度 平均o(n^2) 最好o(n) 最坏o(n^2)
+ 空间复杂度 o(1)
+**/
 public class InsertSort {
 	
-	public static void InsertSort(Comparable[] data){
-		int i,j;
-		for(i=2;i<data.length;i++){
-			if(data[i].compareTo(data[i-1])<0){
-				data[0]=data[i];
-				data[i]=data[i-1];
-				for(j=i-2;data[0].compareTo(data[j])<0;j--)
-					data[j+1]=data[j];
-				data[j+1]=data[0];								
-			}
-		}
-	}
+     public static void InsertSort(Comparable[] data){
+        for(int i=2; i<data.length ;i++){
+            if(data[i].compareTo(data[i-1]) < 0){
+                data[0] = data[i];
+                data[i] = data[i-1];
+                int j;
+                for(j=i-2; data[0].compareTo(data[j]) <0 ;j--){
+                    data[j+1] = data[j];
+                }
+                data[j+1] = data[0];
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2 };
-		InsertSort(c);
-		for (int i = 1; i <c.length; i++)
-			System.out.println("��������" + c[i]);
-	}
+    public static void main(String[] args) {
+        Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2 };
+        InsertSort(c);
+        Stream.of(c).skip(1).forEach(System.out::println);
+    }
+
 }
