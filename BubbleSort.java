aeae665d1b81e94c9fdab6999ae26f5def62b506
zxcodestudy <244266602@qq.com>
2019-08-22 07:49:15 +0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,21 +7,22 @@ package Sort;
   稳定排序
 **/
 public class BubbleSort {
-	public static void BubbleSort(Integer []data){
-		int temp=0;
-		for(int i=data.length-1;i>0;i--)
-			for(int j=0;j<i;j++)
-				if(data[j]>data[j+1]){
-					temp=data[j+1];
-					data[j+1]=data[j];
-					data[j]=temp;
-				}	
-	}
+     public static void BubbleSort(Integer[] data) {
+        for (int i = data.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (data[j + 1] < data[j]) {
+                    int tmp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tmp;
+                }
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-        	Integer[] c = {4, 9, 23, 1, 45, 27, 5, 2 };
-        	BubbleSort(c);
-        	Stream.of(c).skip(1).forEach(System.out::println);
-    	}
+    public static void main(String[] args) {
+        Integer[] c = {4, 9, 23, 1, 45, 27, 5, 2};
+        BubbleSort(c);
+        Stream.of(c).forEach(System.out::println);
+    }
 
 }
