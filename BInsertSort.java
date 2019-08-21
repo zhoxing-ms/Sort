91b5483302f57df3831e5183b585d3cfb6151489
zxcodestudy <244266602@qq.com>
2019-08-22 07:50:11 +0800

    Update BInsertSort.java

--- BInsertSort.java
+++ BInsertSort.java
@@ -7,30 +7,32 @@ package Sort;
   稳定排序
 **/
 public class BInsertSort {
-	public static void BInsertSort(Comparable[] data){
-		int i,j;
-		for(i=2;i<data.length;i++){
-			if(data[i].compareTo(data[i-1])<0){
-				data[0]=data[i];
-				int low=1,high=i-1,mid;
-				while(low<=high){
-					mid=(low+high)/2;
-					if(data[0].compareTo(data[mid])>0)
-						low=mid+1;
-					else
-						high=mid-1;
-				}
-				for(j=i-1;j>=high+1;j--)
-					data[j+1]=data[j];
-				data[high+1]=data[0];
-			}
-		}
-	}
 	
-        public static void main(String[] args) {
-        	Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2 };
-        	BInsertSort(c);
-        	Arrays.stream(c).skip(1).forEach(System.out::println);
-   	}
-	
+     public static void BInsertSort(Comparable[] data) {
+        for (int i = 2; i < data.length; i++) {
+            if (data[i].compareTo(data[i - 1]) < 0) {
+                data[0] = data[i];
+                int low = 1, high = i - 1;
+                while (low <= high) {
+                    int mid = low + (high - low) / 2;
+                    if (data[mid].compareTo(data[0]) < 0) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+                for (int j = i - 1; j > high; j--) {
+                    data[j + 1] = data[j];
+                }
+                data[high + 1] = data[0];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2};
+        BInsertSort(c);
+        Arrays.stream(c).skip(1).forEach(System.out::println);
+    }
+
 }
