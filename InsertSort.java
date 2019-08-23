69b279528a791f39baa7070c908847805a3897f2
zxcodestudy <244266602@qq.com>
2019-08-23 23:58:46 +0800

    Update InsertSort.java

--- InsertSort.java
+++ InsertSort.java
@@ -8,22 +8,21 @@ package Sort;
 **/
 public class InsertSort {
 	
-     public static void InsertSort(Comparable[] data){
-        for(int i=2; i<data.length ;i++){
-            if(data[i].compareTo(data[i-1]) < 0){
+     public static void InsertSort(Comparable[] data) {
+        for (int i = 2; i < data.length; i++) {
+            if (data[i].compareTo(data[i - 1]) < 0) {
                 data[0] = data[i];
-                data[i] = data[i-1];
-                int j;
-                for(j=i-2; data[0].compareTo(data[j]) <0 ;j--){
-                    data[j+1] = data[j];
+                int j = i - 1;
+                for (; j > 0 && data[0].compareTo(data[j]) < 0; j--) {
+                    data[j + 1] = data[j];
                 }
-                data[j+1] = data[0];
+                data[j + 1] = data[0];
             }
         }
     }
 
     public static void main(String[] args) {
-        Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2 };
+        Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2};
         InsertSort(c);
         Stream.of(c).skip(1).forEach(System.out::println);
     }
