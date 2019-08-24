1cadd6ef09d5a5a52906b8c83b7d585c126f11ed
zxcodestudy <244266602@qq.com>
2019-08-24 17:35:11 +0800

    Update BInsertSort.java

--- BInsertSort.java
+++ BInsertSort.java
@@ -8,30 +8,31 @@ package Sort;
 **/
 public class BInsertSort {
 	
-     public static void BInsertSort(Comparable[] data) {
-        for (int i = 2; i < data.length; i++) {
-            if (data[i].compareTo(data[i - 1]) < 0) {
-                data[0] = data[i];
-                int low = 1, high = i - 1;
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    if (data[mid].compareTo(data[0]) < 0) {
-                        low = mid + 1;
-                    } else {
-                        high = mid - 1;
-                    }
-                }
-                for (int j = i - 1; j > high; j--) {
-                    data[j + 1] = data[j];
-                }
-                data[high + 1] = data[0];
+    public static void binsertSort(Comparable[] data) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[i].compareTo(data[i - 1]) > 0) {
+                continue;
             }
+            Comparable tmp = data[i];
+            int low = 1, high = i - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (data[mid].compareTo(tmp) < 0) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            for (int j = i - 1; j > high; j--) {
+                data[j + 1] = data[j];
+            }
+            data[high + 1] = tmp;
         }
     }
 
     public static void main(String[] args) {
-        Integer[] c = {null, 4, 9, 23, 1, 45, 27, 5, 2};
-        BInsertSort(c);
+        Integer[] c = {4, 9, 23, 1, 45, 27, 5, 2};
+        binsertSort(c);
         Arrays.stream(c).skip(1).forEach(System.out::println);
     }
 
