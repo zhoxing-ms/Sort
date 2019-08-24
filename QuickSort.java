d7575c30d3e421350341723072352ba19f2d6b8f
zxcodestudy <244266602@qq.com>
2019-08-24 14:27:50 +0800

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -7,40 +7,40 @@ package Sort;
  不稳定排序
 **/
 public class QuickSort {
-	
-    public static int Partition( Integer[] data,int low,int high){
-       data[0] = data[low];
-       int pivotkey = data[low];
-       while(low < high){
-           //从最后起有比枢轴小的，就赋值给前面的枢轴的位置
-           while(low < high && data[high] >= pivotkey ){
+
+    public static int partition(Integer[] data, int low, int high) {
+        int pivotKey = (low + high) / 2;
+        data[0] = data[pivotKey];
+        data[pivotKey] = data[low];
+        while (low < high) {
+            while (low < high && data[high] >= data[0]) {
                 high--;
-           }
-           data[low] = data[high];
-           //从最前起有比枢轴大的，就赋值给前一次交换过的枢轴的位置
-           while(low < high && data[low] <= pivotkey ){
-               low++;
-           }
-           data[high] = data[low];
-       }
-       data[low] = data[0];
-       return low;
-    }
-
-    public static void QSort(Integer[] data,int low,int high){
-        if(low < high){
-            int p = Partition(data,low,high);
-            QSort(data,low,p-1);
-            QSort(data,p+1,high);
+            }
+            data[low] = data[high];
+            while (low < high && data[low] <= data[0]) {
+                low++;
+            }
+            data[high] = data[low];
         }
+        data[low] = data[0];
+        return low;
     }
 
-    public static void QuickSort(Integer[] c){
-        QSort(c,1,c.length-1);
+    public static void Qsort(Integer[] data, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int pivot = partition(data, low, high);
+        Qsort(data, low, pivot - 1);
+        Qsort(data, pivot + 1, high);
+    }
+
+    public static void QuickSort(Integer[] data) {
+        Qsort(data, 0, data.length - 1);
     }
 
     public static void main(String[] args) {
-        Integer[] c = {null,23, 4, 9, 23, 1, 45, 27, 5, 2  };
+        Integer[] c = {null, 23, 4, 9, 23, 1, 45, 27, 5, 2};
         QuickSort(c);
         Stream.of(c).skip(1).forEach(System.out::println);
     }
