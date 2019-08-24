91b9d6212dff666ea95e7c0c23bf2288d060b48c
zxcodestudy <244266602@qq.com>
2019-08-24 16:51:45 +0800

    Update SelectSort.java

--- SelectSort.java
+++ SelectSort.java
@@ -8,26 +8,25 @@ package Sort;
  **/
 public class SelectSort {
 	
-    public static void SelectSort(Integer []data){
-        int minIndex,temp;
-        for(int i=0; i< data.length ;i++){
-            minIndex = i;
-            for(int j=i+1;j < data.length;j++){
-                if(data[j] < data[minIndex]){
+    public static void selectSort(Integer[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[minIndex]) {
                     minIndex = j;
                 }
             }
-            if(minIndex != i){
-                temp = data[i];
-                data[i] = data[minIndex];
-                data[minIndex] = temp;
+            if (minIndex != i) {
+                int tmp = data[minIndex];
+                data[minIndex] = data[i];
+                data[i] = tmp;
             }
         }
     }
 
     public static void main(String[] args) {
-        Integer[] c = {4, 9, 23, 1, 45, 27, 5, 2 };
-        SelectSort(c);
+        Integer[] c = {4, 9, 23, 1, 45, 27, 5, 2};
+        selectSort(c);
         Stream.of(c).forEach(System.out::println);
     }
 	
